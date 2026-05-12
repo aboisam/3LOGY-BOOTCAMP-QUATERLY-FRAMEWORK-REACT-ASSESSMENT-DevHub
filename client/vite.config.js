5de7eb8acdebc0825bd4751a4b608d221acb9989
aboisam <samsonaboi10@gmail.com>
2026-05-12 20:32:09 +0100

    update on vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+/** @format */
 
-// https://vite.dev/config/
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:5259", // ← change to your backend's port
+        changeOrigin: true,
+      },
+    },
+  },
+});
